85ec0e8ad1aa27cfbac55933a8bb13fda093e949
Koo Kin-Wai <kkoo@redhat.com>
2020-02-21 14:25:48 +0800

    Send curl requests to ingress gateway if gateway object exists

--- curl.sh
+++ curl.sh
@@ -2,7 +2,18 @@
 
 PROJ=demo
 
+if [ $(oc get gateway frontend-gateway -n $PROJ --no-headers 2>/dev/null | wc -l) -lt 1 ]; then
+  GATEWAY=$(oc get route/frontend -n $PROJ -o jsonpath='{.spec.host}')
+  echo "gateway object does not exist"
+else
+  GATEWAY=$(oc get route/istio-ingressgateway -n istio-system -o jsonpath='{.spec.host}')
+  echo "gateway object exists"
+fi
+
+GATEWAY='http://'${GATEWAY}
+echo "sending requests to $GATEWAY"
+
 while true; do
-  curl $(oc get route/frontend -n $PROJ -o jsonpath='{"http://"}{.spec.host}')
+  curl $GATEWAY
   sleep 2
 done
